9f7164fe2e246390cb9315e9edff5101004f1531
Emanuell Carvalho <emanuell.carvalho.pires@gmail.com>
2024-03-13 08:35:19 -0300

    Feito a página de cadastro
    além de ajeitar o HTML do checkbox
    fiz outra página para o cadastro

--- EstudanteIF.js
+++ EstudanteIF.js
@@ -1,6 +1,8 @@
 // Obtém referências para a checkbox e a div
 const minhaCaixaDeSelecao = document.getElementById('exampleCheck1');
 const minhaDiv = document.getElementById('SouEstudanteHIDE');
+const FieldsetCheck = document.getElementById('FieldsetCheck');
+const documentoMatricula = document.getElementById('documentoMatricula');
 
 // Adiciona um evento de alteração à checkbox
 minhaCaixaDeSelecao.addEventListener('change', function() {
@@ -13,3 +15,16 @@ minhaCaixaDeSelecao.addEventListener('change', function() {
     minhaDiv.style.display = 'none';
   }
 });
+
+// Adiciona um evento de alteração à checkbox
+FieldsetCheck.addEventListener('change', function() {
+  // Verifica se a checkbox está marcada
+  if (FieldsetCheck.checked) {
+    // Se estiver marcada, mostra a div
+    documentoMatricula.style.display = 'block';
+  } else {
+    // Se não estiver marcada, oculta a div
+    documentoMatricula.style.display = 'none';
+  }
+});
+
